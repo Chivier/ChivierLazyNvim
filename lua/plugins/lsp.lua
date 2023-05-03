f77f0d9a9d93392e131ee4f679e091d7fbeb2752
Chivier Humber <chivier.humber@outlook.com>
2023-05-03 23:56:56 +0800

    add: plugin configs

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    init = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      keys[#keys + 1] = { "K", false }
+      keys[#keys + 1] = { "<C-k>", "<cmd>lua vim.lsp.buf.hover()<cr>" }
+    end,
+  },
+}
